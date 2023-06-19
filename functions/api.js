dc974c4bb5e0b30073d674957a38a9f3138fa9c8
Jayesh Gohel <jayesh.gohel@gmail.com>
2023-06-19 22:06:07 +0530

    Deploy

--- functions/api.js
+++ functions/api.js
@@ -1,3 +1,10 @@
-export function onRequest(context) {
-  return new Response("Hello World");
+export async function onRequest(context) {
+  const options = {
+    method: "GET",
+    headers: { Authorization: "Basic cnlhdHJhYXBpOlJ5YXRyYWFwaUAxMTE=" },
+  };
+
+  let response = await fetch("https://pullapi.tracknowgps.com/api/v1/index.php/getDevPos", options);
+  let data = await response.json();
+  return new Response(JSON.stringify(data));
 }
